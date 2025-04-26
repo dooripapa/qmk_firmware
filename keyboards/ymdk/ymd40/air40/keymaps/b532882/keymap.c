a7a4faa8f9fa7c793cb437aa5a317ffa3689e920
dooripapa <b532882@hotmail.com>
2025-04-26 21:24:48 +0900

    air40 update

--- keyboards/ymdk/ymd40/air40/keymaps/b532882/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/b532882/keymap.c
@@ -16,40 +16,51 @@
 
 #include QMK_KEYBOARD_H
 
-enum layer_names {
-  _QWERTY,
-  _LOWER,
-  _RAISE,
-  _ADJUST
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_ortho_4x12_2x2u(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_UP,   KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-    [_LOWER] = LAYOUT_ortho_4x12_2x2u(
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-        _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
-    ),
-    [_RAISE] = LAYOUT_ortho_4x12_2x2u(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-        _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
-    ),
-    [_ADJUST] = LAYOUT_ortho_4x12_2x2u(
-        QK_BOOT, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, _______, DB_TOGG,
-        _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, AG_NORM, AG_SWAP, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    )
+[0] = LAYOUT_ortho_4x12_2x2u(
+KC_TAB          , KC_Q      , KC_W        , KC_E         , KC_R        , KC_T         , KC_Y         , KC_U        , KC_I          , KC_O         , KC_P    , KC_BSPC ,
+LCTL_T(KC_ESC)  , LT(6,KC_A), KC_S        , KC_D         , LT(5,KC_F)  , KC_G         , KC_H         , KC_J        , KC_K          , KC_L         , KC_SCLN , KC_ENT  ,
+KC_LSFT         , KC_Z      , KC_X        , KC_C         , KC_V        , KC_B         , KC_N         , KC_M        , KC_COMM       , KC_DOT       , KC_SLSH , KC_RSFT ,
+                  KC_LCTL   , KC_LGUI     , KC_LALT      , KC_SPC      , MO(1)        , LT(2,KC_SPC) , MO(2)       , KC_RALT       , MO(3)        , QK_BOOT
+),
+[1] = LAYOUT_ortho_4x12_2x2u( //CW_TOGG
+    _______  , KC_NO  , KC_NO  , C(S(KC_TAB)) , C(KC_TAB)     , KC_NO   , KC_HOME   , C(KC_PGUP)    , C(KC_PGDN)     , KC_NO   , KC_PGUP  , _______      ,
+    _______  , KC_NO  , KC_NO  , KC_LSFT      , KC_LCTL       , KC_NO   , KC_LEFT   , KC_DOWN       , KC_UP          , KC_RGHT , KC_NO    , C(S(KC_ENT)) ,
+    _______  , KC_NO  , KC_NO  , G(C(KC_LEFT)), G(C(KC_RIGHT)), KC_NO   , KC_END    , G(A(KC_LEFT)) , G(A(KC_RIGHT)) , KC_NO   , KC_PGDN  , _______      ,
+               _______, _______,  _______     , _______       , _______ , KC_NO     , LCTL(KC_INS)  , LSFT(KC_INS)   , _______ , _______ 
+),
+[2] = LAYOUT_ortho_4x12_2x2u(
+    _______ , S(KC_1)   , S(KC_2)  , S(KC_3)   , S(KC_4)   , S(KC_5)    , S(KC_6)    , S(KC_7)    , S(KC_8)    , S(KC_9)  , S(KC_0)    , _______ ,
+    _______ , KC_GRV    , S(KC_GRV), KC_LCBR   , KC_RCBR   , S(KC_BSLS) , KC_EQL     , KC_MINS    , S(KC_QUOT) , KC_QUOT  , S(KC_SCLN) , _______ ,
+    _______ , KC_NO     , KC_NO    , KC_LBRC   , KC_RBRC   , KC_BSLS    , S(KC_EQL)  , S(KC_MINS) , S(KC_COMM) , S(KC_DOT), S(KC_SLSH) , _______ ,
+               _______, _______,  _______     , _______       , _______ , _______   , _______       , _______   , _______ , _______
+),
+[3] = LAYOUT_ortho_4x12_2x2u(
+                        //change signature //rename //run            //debug   //unit test for current cursor position.
+                        //stop                                                             //generate
+    _______ , KC_NO   , KC_NO   , C(KC_F6)         , S(KC_F6), C(S(KC_F10))  , S(KC_F9)  , C(S(KC_F10)), A(KC_INS), KC_NO   , KC_NO   , _______ ,
+    _______ , KC_NO   , C(KC_F2), KC_NO            , KC_NO   , KC_NO         , KC_NO     , KC_NO       , KC_NO    , KC_NO   , KC_NO   , _______ ,
+                                                               //debug spot    //inline    //extract function
+    _______ , KC_NO   , KC_NO   , KC_NO            , KC_NO   , C(KC_F8)      , C(A(KC_N)), C(A(KC_M))  , KC_NO    , KC_NO   , KC_NO   , _______ ,
+               _______, _______,  _______     , _______       , _______ , _______   , _______       , _______        , _______ , _______
+),
+/*Game layout*/
+[4] = LAYOUT_ortho_4x12_2x2u(
+    KC_TAB   , KC_Q     , KC_W   , KC_E     , KC_R     , KC_T      , KC_Y      , KC_U         , KC_I      , KC_O      , KC_P    , KC_BSPC ,
+    KC_LSFT  , KC_A     , KC_S   , KC_D     , KC_F     , KC_G      , KC_H      , KC_J         , KC_K      , KC_L      , KC_SCLN , KC_ENT  ,
+    KC_LCTL  , KC_Z     , KC_X   , KC_C     , KC_V     , KC_B      , KC_N      , KC_M         , KC_COMM   , KC_DOT    , KC_SLSH , KC_RSFT ,
+               _______,  _______, _______   , KC_SPC   , KC_SPC    , KC_RALT   , KC_ESC       , _______   , _______   , _______
+),
+[5] = LAYOUT_ortho_4x12_2x2u(
+    _______  , KC_NO    , KC_NO  , KC_NO    , KC_NO    , KC_NO     , KC_NO        , KC_7      , KC_8      , KC_9      , KC_0    , _______ ,
+    _______  , KC_NO    , KC_NO  , KC_NO    , _______  , KC_NO     , KC_DOT       , KC_4      , KC_5      , KC_6      , KC_NO   , _______ ,
+    _______  , KC_NO    , KC_NO  , KC_NO    , KC_NO    , KC_NO     , KC_COMM      , KC_1      , KC_2      , KC_3      , KC_SLSH , _______ ,
+               _______  , _______,  _______ , _______  , MO(1)     , LT(2,KC_SPC) , _______   , _______   , _______   , _______
+),
+[6] = LAYOUT_ortho_4x12_2x2u(
+    _______  , KC_NO    , KC_NO  , KC_NO    , KC_NO    , KC_NO     , KC_NO     , KC_F7        , KC_F8     , KC_F9      , KC_F10 , _______ ,
+    _______  , _______  , KC_NO  , KC_NO    , KC_NO    , KC_NO     , KC_NO     , KC_F4        , KC_F5     , KC_F6      , KC_F11 , _______ ,
+    _______  , KC_NO    , KC_NO  , KC_NO    , KC_NO    , KC_NO     , KC_NO     , KC_F1        , KC_F2     , KC_F3      , KC_F12 , _______ ,
+               _______  , _______,  _______ , _______  , MO(1)     , LT(2,KC_SPC) , _______   , _______   , _______   , _______
+)
 };
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
